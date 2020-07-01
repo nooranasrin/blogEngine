7a5d610c7eeba7d28b2680ef17b4289fbfc75aa3
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-07-01 13:19:48 +0530

    added port from env variable

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require('./src/router');
 const Database = require('./src/database');
 
+const port = process.env.PORT || 8000;
+
 app.locals.db = new Database('./Data/blog.db');
 app.locals.db.run(
   `create table if not exists blog(name varchar(20), content varchar(10000), date date)`
 );
 
-app.listen(8000, () => console.log('started listening on ', 8000));
+app.listen(port, () => console.log('started listening on ', port));
